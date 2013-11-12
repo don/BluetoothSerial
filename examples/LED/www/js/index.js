b448c8d3019e5a088c6c1abd87f09bfc0ccf4d43
Don Coleman <dcoleman@chariotsolutions.com>
2013-11-11 23:00:09 -0500

    updates from MFNY

--- examples/LED/www/js/index.js
+++ examples/LED/www/js/index.js
@@ -1,5 +1,5 @@
-var macAddress = "00:06:66:4D:AA:AA";
-var bluetoothSerial = cordova.require('bluetoothSerial');
+//var macAddress = "00:06:66:4D:65:22"; // Sparkfun
+var macAddress = "00:13:EF:00:08:8B"; // Seeed Studio
 
 var app = {
     initialize: function() {
@@ -9,52 +9,53 @@ var app = {
         document.addEventListener('deviceready', this.deviceready, false);
     },
     deviceready: function() {
-        
+
         var throttledOnColorChange = _.throttle(app.onColorChange, 200);
 
         //$('input').on('change', app.onColorChange);
         $('input').on('change', throttledOnColorChange);
 
-        connectLabel.addEventListener('touchstart', function(evt) { 
+        connectLabel.addEventListener('touchstart', function(evt) {
             console.log("touchstart");
             app.connect();
         });
 
-        statusLabel.addEventListener('touchstart', function(evt) { 
+        statusLabel.addEventListener('touchstart', function(evt) {
             console.log("status.check");
             app.status();
         });
     },
-    connect: function () {  
-        console.log("connect");      
-        bluetoothSerial.isConnected(function (connected) {
-            console.log("connected is " + connected);
-            if (!connected) {
-                console.log("connecting");                                      
-                bluetoothSerial.connect(
-                    macAddress, 
-                    function() { output.innerHTML = "Connected"; }, 
-                    function (error) { output.innerHTML = error; }
-                );        
-            } else {
+    connect: function () {
+        console.log("connect");
+        bluetoothSerial.isConnected(
+            function() {
                 console.log("already connected");
-            }            
-        });
+            },
+            function () {
+                console.log("connecting");
+                bluetoothSerial.connect(
+                    macAddress,
+                    function() { output.innerHTML = "Connected"; },
+                    function (error) { output.innerHTML = error; }
+                );
+            }
+        );
     },
     status: function () {
-        bluetoothSerial.isConnected(function (connected) {
-            if (connected) {
+        bluetoothSerial.isConnected(
+            function() {
                 output.innerHTML = "Connected!";
-            } else {
-                output.innerHTML = "Not Connected";                
+            },
+            function() {
+                output.innerHTML = "Not Connected";
             }
-        });
+        );
     },
-    onColorChange: function (evt) { 
+    onColorChange: function (evt) {
         var c = app.getColor();
         output.innerHTML = c;
         previewColor.style.backgroundColor = "rgb(" + c + ")";
-        app.sendToArduino(c);                    
+        app.sendToArduino(c);
     },
     getColor: function () {
         var color = [];
@@ -62,7 +63,7 @@ var app = {
         color.push(green.value);
         color.push(blue.value);
         return color.join(',');
-    },    
+    },
     sendToArduino: function(c) {
         bluetoothSerial.write("c" + c + "\n");
     }
