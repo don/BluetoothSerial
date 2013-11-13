89ffa9a97b55cf392308bfc129a6ca43ccc8a7f8
Don Coleman <dcoleman@chariotsolutions.com>
2013-11-13 00:39:08 -0500

    cleanup sketch

--- examples/LED/Arduino/PhoneGapLED/PhoneGapLED.ino
+++ examples/LED/Arduino/PhoneGapLED/PhoneGapLED.ino
@@ -11,54 +11,50 @@ SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = pin number (most are valid)
 // Parameter 3 = pixel type flags
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, neoPixelPin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(60, neoPixelPin, NEO_GRB + NEO_KHZ800);
 uint16_t color;
 
 void setup() {
   Serial.begin(9600);
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'.
+  pixels.begin();
+  pixels.show(); // Initialize all pixels to 'off'.
   setupBluetooth();
   Serial.println("Hello."); 
 }
 
 void loop() {
   // Assumes a string in from the serial port like so:
-  // s ledNumber, brightness \n
-  // for example: "c255,0,0\n":
-  // for example: "c0,0,255\n":
+  // c red, green, blue \n
+  // for example: "c255,0,0\n" shows red
+  // for example: "c0,0,255\n" shows blue
 
   int red = 0;
   int green = 0;
   int blue = 0;
 
   if (bluetooth.find("c")) {
-    red = bluetooth.parseInt(); // parses numeric characters before the comma
+    red = bluetooth.parseInt();  // parses numeric characters before the comma
     green = bluetooth.parseInt();// parses numeric characters after the comma
-    blue = bluetooth.parseInt(); // parses numeric characters before the comma
+    blue = bluetooth.parseInt(); // parses numeric characters after the comma
     
-    // print the results back to the sender:
-    Serial.print("Setting color to: " );
-    Serial.print(red);
-    Serial.print(", ");
-    Serial.print(green);
-    Serial.print(", ");
+    Serial.print("Setting color to: " ); // print to console for debugging
+    Serial.print(red);Serial.print(", ");
+    Serial.print(green);Serial.print(", ");
     Serial.println(blue);
-
-    // set the LED:
-    solid(red, green, blue);
+    
+    showColor(red, green, blue);
   }
 }
 
-void solid(int red, int green, int blue) {
-  uint32_t c = strip.Color(red, green, blue);
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
+void showColor(int red, int green, int blue) {
+  uint32_t c = pixels.Color(red, green, blue);
+  for(uint16_t i=0; i<pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, c);
   }  
-  strip.show();
+  pixels.show();
 }
 
-// init for Sparkfun radio
+// init for Sparkfun radio (works for BLEmini too)
 void setupBluetooth() {
   bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
   bluetooth.print("$$$");  // Enter command mode
