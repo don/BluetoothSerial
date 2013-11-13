febe557ad92fbe65cb99add790bff2997c652a64
Don Coleman <dcoleman@chariotsolutions.com>
2013-11-12 20:07:00 -0500

    ios and topcoat

--- examples/LED/www/js/index.js
+++ examples/LED/www/js/index.js
@@ -1,5 +1,6 @@
-var macAddress = "00:06:66:4D:65:22"; // Sparkfun
-//var macAddress = "00:13:EF:00:08:8B"; // Seeed Studio
+/* jshint quotmark: false, unused: vars */
+/* global cordova, bluetoothSerial, _, connectButton, listButton, deviceList, previewColor, red, green, blue, disconnectButton, connectionScreen, colorScreen, rgbText, messageDiv */
+'use strict';
 
 var app = {
     initialize: function() {
@@ -7,53 +8,55 @@ var app = {
     },
     bind: function() {
         document.addEventListener('deviceready', this.deviceready, false);
+        colorScreen.hidden = true;
     },
     deviceready: function() {
+
+        // wire buttons to functions
+        connectButton.ontouchstart = app.connect;
+        listButton.ontouchstart = app.list;
+        disconnectButton.ontouchstart = app.disconnect;
 
         var throttledOnColorChange = _.throttle(app.onColorChange, 200);
 
         //$('input').on('change', app.onColorChange);
         $('input').on('change', throttledOnColorChange);
-
-        connectLabel.addEventListener('touchstart', function(evt) {
-            console.log("touchstart");
-            app.connect();
-        });
-
-        statusLabel.addEventListener('touchstart', function(evt) {
-            console.log("status.check");
-            app.status();
-        });
+    },
+    list: function(event) {
+        deviceList.firstChild.innerHTML = "Discovering...";
+        app.setStatus("Looking for Bluetooth Devices...");
+        
+        bluetoothSerial.list(app.ondevicelist, app.generateFailureFunction("List Failed"));
     },
     connect: function () {
         console.log("connect");
-        bluetoothSerial.isConnected(
-            function() {
-                console.log("already connected");
-            },
-            function () {
-                console.log("connecting");
-                bluetoothSerial.connect(
-                    macAddress,
-                    function() { output.innerHTML = "Connected"; },
-                    function (error) { output.innerHTML = error; }
-                );
-            }
-        );
+        var device = deviceList[deviceList.selectedIndex].value;
+        app.disable(connectButton);
+        app.setStatus("Connecting...");
+        console.log("Requesting connection to " + device);
+        bluetoothSerial.connect(device, app.onconnect, app.ondisconnect);
     },
-    status: function () {
-        bluetoothSerial.isConnected(
-            function() {
-                output.innerHTML = "Connected!";
-            },
-            function() {
-                output.innerHTML = "Not Connected";
-            }
-        );
+    disconnect: function(event) {
+        if (event) {
+            event.preventDefault();
+        }
+
+        app.setStatus("Disconnecting...");
+        bluetoothSerial.disconnect(app.ondisconnect);
+    },
+    onconnect: function() {
+        connectionScreen.hidden = true;
+        colorScreen.hidden = false;
+        app.setStatus("Connected.");
+    },
+    ondisconnect: function() {
+        connectionScreen.hidden = false;
+        colorScreen.hidden = true;
+        app.setStatus("Disconnected.");
     },
     onColorChange: function (evt) {
         var c = app.getColor();
-        output.innerHTML = c;
+        rgbText.innerText = c;
         previewColor.style.backgroundColor = "rgb(" + c + ")";
         app.sendToArduino(c);
     },
@@ -66,5 +69,71 @@ var app = {
     },
     sendToArduino: function(c) {
         bluetoothSerial.write("c" + c + "\n");
+    },
+    timeoutId: 0,
+    setStatus: function(status) {
+        if (app.timeoutId) {
+            clearTimeout(app.timeoutId);
+        }
+        messageDiv.innerText = status;
+        app.timeoutId = setTimeout(function() { messageDiv.innerText = ""; }, 4000);
+    },
+    ondevicelist: function(devices) {
+        var option;
+
+        // remove existing devices
+        deviceList.innerHTML = "";
+        app.setStatus("");
+
+        devices.forEach(function(device) {
+            option = document.createElement('option');
+            if (device.hasOwnProperty("uuid")) {
+                option.value = device.uuid;
+            } else if (device.hasOwnProperty("address")) {
+                option.value = device.address;
+            } else {
+                option.value = "ERROR " + JSON.stringify(device);
+            }
+            option.innerHTML = device.name;
+            deviceList.appendChild(option);
+        });
+
+        if (devices.length === 0) {
+
+            option = document.createElement('option');
+            option.innerHTML = "No Bluetooth Devices";
+            deviceList.appendChild(option);
+
+            if (cordova.platformId === "ios") { // BLE
+                app.setStatus("No Bluetooth Peripherals Discovered.");
+            } else { // Android
+                app.setStatus("Please Pair a Bluetooth Device.");
+            }
+
+            app.disable(connectButton);
+            listButton.style.display = "";
+        } else {
+            app.enable(connectButton);
+            listButton.style.display = "none";
+            app.setStatus("Found " + devices.length + " device" + (devices.length === 1 ? "." : "s."));
+        }
+    },
+    enable: function(button) {
+        button.className = button.className.replace(/\bis-disabled\b/g,'');
+    },
+    disable: function(button) {
+        if (!button.className.match(/is-disabled/)) {
+            button.className += " is-disabled";
+        }
+    },
+    generateFailureFunction: function(message) {
+        var func = function(reason) {
+            var details = "";
+            if (reason) {
+                details += ": " + JSON.stringify(reason);
+            }
+            app.setStatus(message + details);
+        };
+        return func;
     }
 };
