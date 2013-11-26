f26412c2e1a4b9bfd5ea4a46fdcee37b0414c9fa
Don Coleman <dcoleman@chariotsolutions.com>
2013-11-26 18:56:35 -0500

    Fixes #26 - multiple delimiters in received data

--- src/android/com/megster/cordova/BluetoothSerial.java
+++ src/android/com/megster/cordova/BluetoothSerial.java
@@ -30,14 +30,14 @@ public class BluetoothSerial extends CordovaPlugin {
     private static final String CONNECT_INSECURE = "connectInsecure";
     private static final String DISCONNECT = "disconnect";
     private static final String WRITE = "write";
-    private static final String AVAILABLE  = "available";
-    private static final String READ  = "read";
-    private static final String READ_UNTIL  = "readUntil";
+    private static final String AVAILABLE = "available";
+    private static final String READ = "read";
+    private static final String READ_UNTIL = "readUntil";
     private static final String SUBSCRIBE = "subscribe";
-    private static final String UNSUBSCRIBE  = "unsubscribe";
+    private static final String UNSUBSCRIBE = "unsubscribe";
     private static final String IS_ENABLED = "isEnabled";
-    private static final String IS_CONNECTED  = "isConnected";
-    private static final String CLEAR  = "clear";
+    private static final String IS_CONNECTED = "isConnected";
+    private static final String CLEAR = "clear";
 
     // callbacks
     private CallbackContext connectCallback;
@@ -276,6 +276,8 @@ public class BluetoothSerial extends CordovaPlugin {
             PluginResult result = new PluginResult(PluginResult.Status.OK, data);
             result.setKeepCallback(true);
             dataAvailableCallback.sendPluginResult(result);
+
+            sendDataToSubscriber();
         }
     }
 
