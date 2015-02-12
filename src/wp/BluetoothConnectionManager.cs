9575978bea007a560c13f278496c5a1f43813743
Don Coleman <don.coleman@gmail.com>
2015-02-11 19:44:21 -0500

    implementing wp8

---
+++ src/wp/BluetoothConnectionManager.cs
@@ -0,0 +1,149 @@
+﻿// originally based on http://developer.nokia.com/community/wiki/Windows_Phone_8_communicating_with_Arduino_using_Bluetooth
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+
+namespace BluetoothConnectionManager
+{
+    /// <summary>
+    /// Class to control the bluetooth connection to the Arduino.
+    /// </summary>
+    public class ConnectionManager
+    {
+        /// <summary>
+        /// Socket used to communicate with Arduino.
+        /// </summary>
+        private StreamSocket socket;
+
+        /// <summary>
+        /// DataWriter used to send commands easily.
+        /// </summary>
+        private DataWriter dataWriter;
+
+        /// <summary>
+        /// DataReader used to receive messages easily.
+        /// </summary>
+        private DataReader dataReader;
+
+        /// <summary>
+        /// Thread used to keep reading data from socket.
+        /// </summary>
+        private BackgroundWorker dataReadWorker;
+
+        /// <summary>
+        /// Delegate used by event handler.
+        /// </summary>
+        /// <param name="message">The message received.</param>
+        public delegate void MessageReceivedHandler(string message);
+
+        /// <summary>
+        /// Event fired when a new message is received from Arduino.
+        /// </summary>
+        public event MessageReceivedHandler MessageReceived;
+
+        /// <summary>
+        /// Initialize the manager, should be called in OnNavigatedTo of main page.
+        /// </summary>
+        public void Initialize()
+        {
+            socket = new StreamSocket();
+            dataReadWorker = new BackgroundWorker();
+            dataReadWorker.WorkerSupportsCancellation = true;
+            dataReadWorker.DoWork += new DoWorkEventHandler(ReceiveMessages);
+        }
+
+        /// <summary>
+        /// Finalize the connection manager, should be called in OnNavigatedFrom of main page.
+        /// </summary>
+        public void Terminate()
+        {
+            if (socket != null)
+            {
+                socket.Dispose();
+            }
+            if (dataReadWorker != null)
+            {
+                dataReadWorker.CancelAsync();
+            }
+        }
+
+        /// <summary>
+        /// Connect to the given host device.
+        /// </summary>
+        /// <param name="deviceHostName">The host device name.</param>
+        public async void Connect(HostName deviceHostName)
+        {
+            if (socket != null) // TODO is the socket is null, we need to return an error to cordova
+            {
+                await socket.ConnectAsync(deviceHostName, "1");
+                dataReader = new DataReader(socket.InputStream);
+                dataReadWorker.RunWorkerAsync();
+                dataWriter = new DataWriter(socket.OutputStream);
+            }
+        }
+
+        /// <summary>
+        /// Receive messages from the Arduino through bluetooth.
+        /// </summary>
+        private async void ReceiveMessages(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                while (true)
+                {
+                    // Read first byte (length of the subsequent message, 255 or less). 
+                    uint sizeFieldCount = await dataReader.LoadAsync(1);
+                    if (sizeFieldCount != 1)
+                    {
+                        // The underlying socket was closed before we were able to read the whole data. 
+                        return;
+                    }
+
+                    // Read the message. 
+                    uint messageLength = dataReader.ReadByte();
+                    uint actualMessageLength = await dataReader.LoadAsync(messageLength);
+                    if (messageLength != actualMessageLength)
+                    {
+                        // The underlying socket was closed before we were able to read the whole data. 
+                        return;
+                    }
+                    // Read the message and process it.
+                    string message = dataReader.ReadString(actualMessageLength);
+                    MessageReceived(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Send command to the Arduino through bluetooth.
+        /// </summary>
+        /// <param name="command">The sent command.</param>
+        /// <returns>The number of bytes sent</returns>
+        public async Task<uint> SendCommand(string command)
+        {
+            uint sentCommandSize = 0;
+            if (dataWriter != null)
+            {
+                uint commandSize = dataWriter.MeasureString(command);
+                dataWriter.WriteByte((byte)commandSize);
+                sentCommandSize = dataWriter.WriteString(command);
+                await dataWriter.StoreAsync();
+            }
+            return sentCommandSize;
+        }
+    }
+}
+
--- src/wp/BluetoothSerial.cs
+++ src/wp/BluetoothSerial.cs
@@ -7,6 +7,11 @@ using Windows.Networking.Proximity;
 using WPCordovaClassLib.Cordova;
 using WPCordovaClassLib.Cordova.Commands;
 using WPCordovaClassLib.Cordova.JSON;
+using Microsoft.Phone.Tasks;
+
+using BluetoothConnectionManager;
+using Windows.Networking;
+using System.Text;
 
 public class BluetoothSerial : BaseCommand
 {
@@ -33,6 +38,10 @@ public class BluetoothSerial : BaseCommand
     - (void)readRSSI:(CDVInvokedUrlCommand *)command;
     */
 
+    private ConnectionManager connectionManager;
+    private string rawDataCallbackId;
+    private string subscribeCallbackId;
+
     // no args
     public async void list(string args)
     {
@@ -49,6 +58,81 @@ public class BluetoothSerial : BaseCommand
         // convert to serializable format
         var pairedDeviceList = new List<PairedDeviceInfo>(pairedDevices.Select(x => new PairedDeviceInfo(x)));
         DispatchCommandResult(new PluginResult(PluginResult.Status.OK, pairedDeviceList));
+    }
+
+    public void connect(string args)
+    {
+        string macAddress = JsonHelper.Deserialize<string[]>(args)[0];
+
+        connectionManager = new ConnectionManager();
+        connectionManager.Initialize(); // TODO can't we put this in the constructor?
+        connectionManager.MessageReceived += connectionManager_MessageReceived;
+
+        HostName deviceHostName = new HostName(macAddress);
+
+        connectionManager.Connect(deviceHostName);
+        DispatchCommandResult(new PluginResult(PluginResult.Status.OK));
+    }
+
+    private void connectionManager_MessageReceived(string message)
+    {
+        Debug.WriteLine(message);
+        if (subscribeCallbackId != null)
+        {
+            DispatchCommandResult(new PluginResult(PluginResult.Status.OK, message), subscribeCallbackId);
+        }
+    }
+
+    public void disconnect(string args)
+    {
+        connectionManager.Terminate();
+
+        DispatchCommandResult(new PluginResult(PluginResult.Status.OK));
+    }
+
+    public void subscribe(string args)
+    {
+        var arguments = JsonHelper.Deserialize<string[]>(args);
+        var token = arguments[0];
+        subscribeCallbackId = arguments[1];
+    }
+
+    public void unsubscribe(string args)
+    {
+
+    }
+
+    public void subscribeRaw(string args)
+    {
+        rawDataCallbackId = JsonHelper.Deserialize<string[]>(args)[0];
+    }
+
+    public void unsubscribeRaw(string args)
+    {
+        rawDataCallbackId = null;
+    }
+
+    public async void write(string args)
+    {
+        // TODO need to handle bytes
+        string encodedMessageBytes = JsonHelper.Deserialize<string[]>(args)[0];
+        byte[] data = Convert.FromBase64String(encodedMessageBytes);
+
+        // Using a string for now until I fix the underlying class
+        var message = Encoding.UTF8.GetString(data, 0, data.Length);
+
+        var result = await connectionManager.SendCommand(message);
+
+        // TODO handle bad cases
+        DispatchCommandResult(new PluginResult(PluginResult.Status.OK));
+    }
+
+    // TODO new API, need to add JavaScript
+    public void showBluetoothConnectionSettings(string args)
+    {
+        ConnectionSettingsTask connectionSettingsTask = new ConnectionSettingsTask();
+        connectionSettingsTask.ConnectionSettingsType = ConnectionSettingsType.Bluetooth;
+        connectionSettingsTask.Show();
     }
 
     [DataContract]
