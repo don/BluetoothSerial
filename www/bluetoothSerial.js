c7f425b553abebab772bdeaff2684e048761211b
Louis Deguillaume <louis.deguillaume@etu.u-bordeaux.fr>
2015-04-15 11:15:39 +0200

    add support for old devices that doesn't get Uint8Array and window.btoa(data)

--- www/bluetoothSerial.js
+++ www/bluetoothSerial.js
@@ -125,10 +125,85 @@ module.exports = {
 
 };
 
-var stringToArrayBuffer = function(str) {
-    var ret = new Uint8Array(str.length);
-    for (var i = 0; i < str.length; i++) {
-        ret[i] = str.charCodeAt(i);
+// make sure that the client knows Uint8Array and btoa, if not, create them
+(function() {
+  try {
+    var a = new Uint8Array(1);
+    return; //no need
+  } catch(e) { console.log("Your device does not support UintArray, creating it for you."); }
+ 
+  function subarray(start, end) {
+    return this.slice(start, end);
+  }
+ 
+  function set_(array, offset) {
+    if (arguments.length < 2) offset = 0;
+    for (var i = 0, n = array.length; i < n; ++i, ++offset)
+      this[offset] = array[i] & 0xFF;
+  }
+ 
+  // we need typed arrays
+  function TypedArray(arg1) {
+    var result;
+    if (typeof arg1 === "number") {
+       result = new Array(arg1);
+       for (var i = 0; i < arg1; ++i)
+         result[i] = 0;
+    } else
+       result = arg1.slice(0);
+    result.subarray = subarray;
+    result.buffer = result;
+    result.byteLength = result.length;
+    result.set = set_;
+    if (typeof arg1 === "object" && arg1.buffer)
+      result.buffer = arg1.buffer;
+ 
+    return result;
+  }
+ 
+  window.Uint8Array = TypedArray;
+  window.Uint32Array = TypedArray;
+  window.Int32Array = TypedArray;
+})();
+ 
+ 
+(function() {
+  if ("response" in XMLHttpRequest.prototype ||
+      "mozResponseArrayBuffer" in XMLHttpRequest.prototype || 
+      "mozResponse" in XMLHttpRequest.prototype ||
+      "responseArrayBuffer" in XMLHttpRequest.prototype)
+    return;
+  Object.defineProperty(XMLHttpRequest.prototype, "response", {
+    get: function() {
+      return new Uint8Array( new VBArray(this.responseBody).toArray() );
     }
-    return ret.buffer;
+  });
+})();
+ 
+(function() {
+  if ("btoa" in window)
+    return;
+
+  console.log("Your device does not support btoa, creating it for you.");
+ 
+  var digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+ 
+  window.btoa = function(chars) {
+    var buffer = "";
+    var i, n;
+    for (i = 0, n = chars.length; i < n; i += 3) {
+      var b1 = chars.charCodeAt(i) & 0xFF;
+      var b2 = chars.charCodeAt(i + 1) & 0xFF;
+      var b3 = chars.charCodeAt(i + 2) & 0xFF;
+      var d1 = b1 >> 2, d2 = ((b1 & 3) << 4) | (b2 >> 4);
+      var d3 = i + 1 < n ? ((b2 & 0xF) << 2) | (b3 >> 6) : 64;
+      var d4 = i + 2 < n ? (b3 & 0x3F) : 64;
+      buffer += digits.charAt(d1) + digits.charAt(d2) + digits.charAt(d3) + digits.charAt(d4);
+    }
+    return buffer;
+  }; 
+})();
+
+var stringToArrayBuffer = function(str) {
+    return windows.btoa(str);
 };
