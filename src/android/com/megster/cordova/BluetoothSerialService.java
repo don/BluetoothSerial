0210bcb5905d50766441416512512378f2f9df19
Luccas Robert <luccasrobert@hotmail.com>
2018-04-20 16:04:01 -0300

    Use correct UUIDs when connecting

--- src/android/com/megster/cordova/BluetoothSerialService.java
+++ src/android/com/megster/cordova/BluetoothSerialService.java
@@ -348,11 +348,11 @@ public class BluetoothSerialService {
             // Get a BluetoothSocket for a connection with the given BluetoothDevice
             try {
                 if (secure) {
-                    // tmp = device.createRfcommSocketToServiceRecord(MY_UUID_SECURE);
-                    tmp = device.createRfcommSocketToServiceRecord(UUID_SPP);
+                    tmp = device.createRfcommSocketToServiceRecord(MY_UUID_SECURE);
+                    //tmp = device.createRfcommSocketToServiceRecord(UUID_SPP);
                 } else {
-                    //tmp = device.createInsecureRfcommSocketToServiceRecord(MY_UUID_INSECURE);
-                    tmp = device.createInsecureRfcommSocketToServiceRecord(UUID_SPP);
+                    tmp = device.createInsecureRfcommSocketToServiceRecord(MY_UUID_INSECURE);
+                    //tmp = device.createInsecureRfcommSocketToServiceRecord(UUID_SPP);
                 }
             } catch (IOException e) {
                 Log.e(TAG, "Socket Type: " + mSocketType + "create() failed", e);
